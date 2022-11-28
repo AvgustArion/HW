d7155e63ff9d464b1b8a0a84779790951d23e099
Avgust <dr.impu1s@yandex.ru>
2022-11-28 15:36:45 +0500

    Первое ДЗ

---
+++ HW2/1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трёхзначное число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int a = number % 100;
+int b = a / 10;
+Console.WriteLine(b);
