28eeb5543ddd6fb7645589bd89b3014e347fee2a
Avgust <dr.impu1s@yandex.ru>
2022-12-06 02:53:44 +0500

    Третье ДЗ

---
+++ HW4/3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Clear();
+int lenth = MAS("Ввведите длинну массива: ");
+int[] random = new int[lenth];
+for (int i = 0; i < random.Length; i++)
+{
+    random[i] = new Random().Next(1,9);
+    Console.Write(random[i] + " ");
+}
+int MAS(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
